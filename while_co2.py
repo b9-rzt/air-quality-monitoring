a258fb1acc35b18445e55525f277c8c051240a70
Florian Ott <florianottj@icloud.com>
2022-03-08 13:12:23 +0100

    komments in scripts

--- while_co2.py
+++ while_co2.py
@@ -2,11 +2,20 @@ import time, sys
 import board
 import adafruit_ccs811
 from subprocess import Popen, PIPE
+
+# Token for authentication in Thingsboard
 token="vdRsH8PPe8nme153ERpC"
+
+#Thingsboard Ip
 host="localhost"
+
+# Thingsboard MQTT Port
 port="1883"
+
+# Intervall of sending sensor data in minutes 
 delaytime = int(sys.argv[1])*60
 
+# Function to run commands in the commandshell on linux
 def console(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
     out, err = p.communicate()
@@ -22,9 +31,10 @@ while not ccs811.data_ready:
 while True:
     #print("CO2: {} PPM, TVOC: {} PPB".format(ccs811.eco2, ccs811.tvoc))
     co2=ccs811.eco2
-    if(co2 is not None) and (co2>=400) and (co2<5000):
-        data='{"Co2":'+str(co2)+'}'
-        publish='mosquitto_pub -d -q 1 -h "'+host+'" -p "'+port+'" -t "v1/devices/me/telemetry" -u "'+token+'" -m '+ data
+    if(co2 is not None) and (co2>=400) and (co2<5000): # if co2 is between 400 and 5000 else the value is wrong
+        data='{"Co2":'+str(co2)+'}' # put sensor data in a json format
+        # send the data via mqtt to thingsboard
+        publish='mosquitto_pub -d -q 1 -h "'+host+'" -p "'+port+'" -t "v1/devices/me/telemetry" -u "'+token+'" -m '+ data 
         console(publish)
         time.sleep(delaytime)
     else:
--- while_dht.py
+++ while_dht.py
@@ -3,11 +3,20 @@ import board
 import sys
 import adafruit_dht
 from subprocess import Popen, PIPE
+
+# Token for authentication in Thingsboard
 token="vdRsH8PPe8nme153ERpC"
+
+#Thingsboard Ip
 host="localhost"
+
+# Thingsboard MQTT Port
 port="1883"
+
+# Intervall of sending sensor data in minutes 
 delaytime = int(sys.argv[1])*60
 
+# Function to run commands in the commandshell on linux
 def console(cmd):
     p = Popen(cmd, shell=True, stdout=PIPE)
     out, err = p.communicate()
@@ -15,27 +24,21 @@ def console(cmd):
  
 # Initial the dht device, with data pin connected to:
 # dhtDevice = adafruit_dht.DHT22(board.D4)
- 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
 dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
  
 while True:
     try:
         # Print the values to the serial port
         temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
-        data='{"Temperature":'+str(temperature_c)+',"Humidity":'+str(humidity)+'}'
 
+        # put sensor data in a json format
+        data='{"Temperature":'+str(temperature_c)+',"Humidity":'+str(humidity)+'}'
+        
+        # send the data via mqtt to thingsboard
         publish='mosquitto_pub -d -q 1 -h "'+host+'" -p "'+port+'" -t "v1/devices/me/telemetry" -u "'+token+'" -m '+ data
         console(publish)
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
+
     except RuntimeError as error:
         # Errors happen fairly often, DHT's are hard to read, just keep going
         print(error.args[0])
