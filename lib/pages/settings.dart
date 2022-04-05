53d99e422f47903ca1b381b8a0a902c65f162b72
Florian Ott <florianottj@icloud.com>
2022-04-05 19:55:20 +0200

    update settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/Backend/thingsboard_adapter_client.dart';
-import 'package:myapp/value/settvalue.dart';
-// import 'package:regexed_validator/regexed_validator.dart';
 
 /// Settings Page
 class Settings extends StatefulWidget {
@@ -16,62 +14,7 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   final ThingsboardAdapterClient _c;
   _SettingsState(this._c);
-
-  /// list with settings
-  List<Setting> sett = [];
-
-  /// definition of the most used icons
-  Icon check = const Icon(Icons.check, color: Colors.blue);
-  Icon close = const Icon(Icons.close, color: Colors.red);
-
-  @override
-  void initState() {
-    addSettings();
-    super.initState();
-  }
-
-  /// initiate the settings
-  void addSettings() {
-    /// setting ip address
-    sett.add(Setting("IPAddress", const Icon(Icons.circle_outlined),
-        "IP-Adresse von Thingsboard", (String v) {
-      setState(() {
-        sett[0].icon = check;
-        sett[0].valuetext = v;
-        sett[0].saveSetting(_c);
-        // sett[0].helpertext = "";
-      });
-      // if (validator.ip(v)) {
-      //   setState(() {
-      //     sett[0].icon = check;
-      //     sett[0].valuetext = v;
-      //     sett[0].saveSetting(_c);
-      //     sett[0].helpertext = "";
-      //   });
-      // } else {
-      //   setState(() {
-      //     sett[0].icon = close;
-      //     sett[0].helpertext = "Ivalid IP Address!";
-      //   });
-      // }
-    }, "", _c.sa.getElementwithkey("IPAddress"), 16));
-
-    /// setting username
-    sett.add(Setting("Username", check, "Username:", (String v) {
-      setState(() {
-        sett[1].valuetext = v;
-        sett[1].saveSetting(_c);
-      });
-    }, "", _c.sa.getElementwithkey("Username"), 40));
-
-    /// setting password
-    sett.add(Setting.password("Password", check, "Password:", (String v) {
-      setState(() {
-        sett[2].valuetext = v;
-        sett[2].saveSetting(_c);
-      });
-    }, "", _c.sa.getElementwithkey("Password"), 40));
-  }
+  bool _hidePassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -86,78 +29,113 @@ class _SettingsState extends State<Settings> {
             children: <Widget>[
               const SizedBox(height: 10),
 
-              /// list the settings on the page
-              for (int count
-                  in List.generate(sett.length, (index) => index + 1))
-                textformfieldSettings(context, sett[count - 1]),
+              Form(
+                child: TextFormField(
+                  onChanged: (value) {
+                    _c.sa.updateElementwithKey("IPAddress", value);
+                  },
+                  initialValue: _c.sa.getElementwithkey("IPAddress"),
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    labelText: "IP Addresse von Thingsboard",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color:
+                            Theme.of(context).backgroundColor.withOpacity(0.2),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.dns,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              Form(
+                child: TextFormField(
+                  onChanged: (value) {
+                    _c.sa.updateElementwithKey("Username", value);
+                  },
+                  initialValue: _c.sa.getElementwithkey("Username"),
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: "Username",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color:
+                            Theme.of(context).backgroundColor.withOpacity(0.2),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.supervised_user_circle,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+
+              // Passwordfield
+              Form(
+                child: TextFormField(
+                  onChanged: (value) {
+                    _c.sa.updateElementwithKey("Password", value);
+                  },
+                  keyboardType: TextInputType.text,
+                  obscureText: _hidePassword,
+                  initialValue: _c.sa.getElementwithkey("Password"),
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color:
+                            Theme.of(context).backgroundColor.withOpacity(0.2),
+                      ),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.secondary,
+                      ),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _hidePassword = !_hidePassword;
+                        });
+                      },
+                      color: Theme.of(context)
+                          .colorScheme
+                          .secondary
+                          .withOpacity(0.4),
+                      icon: Icon(_hidePassword
+                          ? Icons.visibility_off
+                          : Icons.visibility),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ));
-  }
-
-  /// label widget
-  Widget label(String text) {
-    return Text(
-      text,
-      style:
-          Theme.of(context).textTheme.subtitle1!.copyWith(color: Colors.black),
-    );
-  }
-
-  /// divider widget
-  Widget div() {
-    return const Divider(
-      color: Colors.black,
-      height: 5,
-    );
-  }
-
-  /// radio button widget for the future
-  // Widget radioText(
-  //     BuildContext context, String text, SettValue groupvalue, int value1) {
-  //   return ListTile(
-  //     title: Text(
-  //       text,
-  //       style: Theme.of(context)
-  //           .textTheme
-  //           .subtitle1!
-  //           .copyWith(color: Colors.black),
-  //     ),
-  //     trailing: Radio(
-  //       value: value1,
-  //       groupValue: groupvalue.value,
-  //       activeColor: const Color.fromARGB(255, 4, 0, 238),
-  //       onChanged: (int? value) {
-  //         setState(() {
-  //           groupvalue.value = value;
-  //         });
-  //       },
-  //     ),
-  //   );
-  // }
-
-  /// textfield for editing the settings
-  Widget textformfieldSettings(BuildContext context, Setting set) {
-    return ListTile(
-        title: TextFormField(
-            obscureText: set.obscure,
-            cursorColor: const Color.fromARGB(255, 0, 4, 255),
-            initialValue: set.valuetext,
-            maxLength: set.maxlength,
-            onChanged: set.changefunction,
-            decoration: inputdeco(set)));
-  }
-
-  /// decoration of the textfields
-  InputDecoration inputdeco(Setting set) {
-    return InputDecoration(
-      labelText: set.settingstext,
-      labelStyle: const TextStyle(
-        color: Color.fromARGB(255, 0, 0, 0),
-      ),
-      suffixIcon: set.icon,
-      helperText: set.helpertext,
-      helperStyle: const TextStyle(color: Color.fromARGB(255, 255, 0, 0)),
-    );
   }
 }
